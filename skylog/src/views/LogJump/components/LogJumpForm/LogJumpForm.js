f66e0547f480158dbb8cec1cc4419f9755dc08d1
anusca <anna.arsentieva@gmail.com>
2020-07-08 08:39:58 -0700

    implement sliders for exit/open and wind

--- skylog/src/views/LogJump/components/LogJumpForm/LogJumpForm.js
+++ skylog/src/views/LogJump/components/LogJumpForm/LogJumpForm.js
@@ -2,33 +2,32 @@ import React, { useState } from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
-import { Card, CardHeader, CardContent, CardActions, Divider, Grid, Button, Select, MenuItem,TextField, FormControl, FormHelperText,InputLabel} from '@material-ui/core';
+import { Card, CardHeader, CardContent, CardActions, Divider, Grid, Button, Select, MenuItem,TextField, FormControl, InputLabel,Typography, Slider} from '@material-ui/core';
 
 import DateTimePicker from './DateTimePicker';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
   formControl: {
-    margin: theme.spacing(0),
+    marginLeft: theme.spacing(1),
     minWidth: 100,
-    maxHeight: 15
   },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
+  dateTime: {
+    marginTop: theme.spacing(-2),
+    marginLeft: theme.spacing(-2),
+  }
 }));
 
 const LogJumpForm = props => {
   const { className, ...rest } = props;
-
   const classes = useStyles();
 
   const [values, setValues] = useState({
-
       id: 18,
       title: "Chelan Formation",
       exit: 1710,
       open: 1200,
+      freefall: "36s",
       jumpDate: 1555016400000,
       type: "freestyle",
       aircraft:"cessna",
@@ -51,30 +50,42 @@ const LogJumpForm = props => {
         <CardHeader subheader="The information can be edited" title="Log Jump" />
         <Divider />
         <CardContent>
-          <Grid container spacing={3} >
+          <Grid container spacing={2} >
             <Grid item  md={2} xs={2}>
-              <TextField label="Jump Number" margin="dense" name="number" onChange={handleChange} required value={values.id} variant="outlined"/>
+              <TextField fullWidth label="Jump Number" margin="dense" name="number" onChange={handleChange} required value={values.id} variant="outlined"/>
             </Grid>
-            <Grid item md={6} xs={12}>
+            <Grid item md={7} xs={12}>
               <TextField fullWidth label="Name your jump" margin="dense" name="title" onChange={handleChange} required value={values.title} variant="outlined" />
             </Grid>
-            <Grid item md={2} xs={2}>
-              <TextField label="Exit" margin="dense" name="email" onChange={handleChange} required value={values.exit} variant="outlined"/>
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <TextField fullWidth label="Open" margin="dense" name="open" onChange={handleChange} type="number" value={values.open} variant="outlined"/>
-            </Grid>
-            <Grid item md={6} xs={12} >
-            <FormControl className={classes.formControl}>
+            <Grid item md={1} xs={6} >
+              <FormControl className={classes.formControl}>
                 <InputLabel>Select Type</InputLabel>
-                <Select labelId="demo-simple-select-label" id="demo-simple-select" value={values.type} onChange={handleChange} autoWidth className={classes.selectEmpty}>
-                <MenuItem value=""><em>Select Type</em></MenuItem>
+                <Select labelId="demo-simple-select-label" id="demo-simple-select" value={values.type} onChange={handleChange} autoWidth >
+                  <MenuItem value=""><em>Select Type</em></MenuItem>
                   <MenuItem value={"freestyle"}>Freestyle</MenuItem>
                   {/* <MenuItem value={20}>Windsuit</MenuItem>
                   <MenuItem value={30}>other</MenuItem> */}
                 </Select>
-               {/* <FormHelperText>Select Type</FormHelperText> */}
-             </FormControl>
+                {/* <FormHelperText>Select Type</FormHelperText> */}
+              </FormControl>
+            </Grid>
+            <Grid item md={1} xs={2}>
+              <TextField fullWidth label="Freefall Time" margin="dense" name="title" onChange={handleChange} required value={values.freefall} variant="outlined" />
+            </Grid>
+            <Grid item md={4} xs={6}>
+              {/* <TextField label="Exit" margin="dense" name="email" onChange={handleChange} required value={values.exit} variant="outlined"/> */}
+              <Typography id="vertical-slider" gutterBottom> Exit(ft)</Typography>
+              <Slider defaultValue={6000}  step={250} marks min={2500} max={16000} valueLabelDisplay="on"/>
+            </Grid>
+            <Grid item md={4} xs={6}>
+              {/* <TextField fullWidth label="Open" margin="dense" name="open" onChange={handleChange} type="number" value={values.open} variant="outlined"/> */}
+              <Typography id="vertical-slider" gutterBottom> Open(ft) </Typography>
+              <Slider defaultValue={4500} step={250} marks min={2000} max={16000} valueLabelDisplay="on"/>
+            </Grid>
+            <Grid item md={4} xs={6} >
+              {/* <TextField fullWidth label="Wind" margin="dense" name="wind" onChange={handleChange}  required value={values.wind} variant="outlined"/> */}
+              <Typography id="vertical-slider" gutterBottom> Wind(mile/h) </Typography>
+              <Slider defaultValue={10}   step={1} marks min={1} max={25} valueLabelDisplay="on"/>
             </Grid>
             <Grid item md={6} xs={12} >
               <TextField fullWidth label="Aircraft" margin="dense" name="aircraft" onChange={handleChange}  required value={values.aircraft} variant="outlined"/>
@@ -82,22 +93,21 @@ const LogJumpForm = props => {
             <Grid item md={6} xs={12} >
               <TextField fullWidth label="Location" margin="dense" name="location" onChange={handleChange}  required value={values.location} variant="outlined"/>
             </Grid>
-            <Grid item md={6} xs={12} >
-              <TextField fullWidth label="Wind" margin="dense" name="wind" onChange={handleChange}  required value={values.wind} variant="outlined"/>
-            </Grid>
             <Grid item md={3} xs={3} >
               <TextField fullWidth label="Equipment" margin="dense" name="equipment" onChange={handleChange}  required value={values.equipment} variant="outlined"/>
             </Grid>
             <Grid item md={3} xs={3} >
               <TextField fullWidth label="Velocity" margin="dense" name="velocity" onChange={handleChange}  required value={values.velocity} variant="outlined"/>
             </Grid>
-            <Grid item md={12} xs={12} >
-              <TextField fullWidth label="Notes" margin="dense" name="notes" onChange={handleChange}  required value={values.notes} variant="outlined"/>
-            </Grid>
             <Grid item md={6} xs={12} >
               {/* <TextField fullWidth label="Select Date" margin="dense" name="date" onChange={handleChange} required select SelectProps={{ native: true }} value={values.date}
                 variant="outlined" >{states.map(option => (<option key={option.value} value={option.value}> {option.label} </option>))}</TextField> */}
+               <div className={classes.dateTime}>
                 <DateTimePicker />
+                </div>
+            </Grid>
+            <Grid item md={12} xs={12} >
+              <TextField fullWidth label="Notes" margin="dense" name="notes" onChange={handleChange}  required value={values.notes} variant="outlined"/>
             </Grid>
             <Grid item md={6} xs={12} >
               <TextField fullWidth label="Signature" margin="dense" name="signature" onChange={handleChange}  required value={values.signature} variant="outlined"/>
