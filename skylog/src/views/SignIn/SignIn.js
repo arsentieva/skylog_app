9edd7a586d4159e408b56da384ff54a538ad1b39
anusca <anna.arsentieva@gmail.com>
2020-07-08 13:01:06 -0700

    implment sign-in

--- skylog/src/views/SignIn/SignIn.js
+++ skylog/src/views/SignIn/SignIn.js
@@ -5,6 +5,7 @@ import validate from 'validate.js';
 import { makeStyles } from '@material-ui/styles';
 import { Grid, Button, IconButton, TextField, Link, Typography} from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { apiBaseUrl } from "../../config";
 
 import { Facebook as FacebookIcon, Google as GoogleIcon } from 'icons';
 
@@ -54,14 +55,10 @@ const useStyles = makeStyles(theme => ({
   },
   quoteText: {
     color: theme.palette.white,
-    fontWeight: 300
-  },
-  name: {
-    marginTop: theme.spacing(3),
-    color: theme.palette.white
-  },
-  bio: {
-    color: theme.palette.white
+    fontWeight: 700,
+    textShadow: "#ABC 1px 0 10px",
+    marginTop:theme.spacing(-17),
+    textTransform: "uppercase"
   },
   contentContainer: {},
   content: {
@@ -163,28 +160,46 @@ const SignIn = props => {
     }));
   };
 
-  const handleSignIn = event => {
+  const handleSignIn =async(event) => {
     event.preventDefault();
-    history.push('/');
+    let email=formState.values.email;
+    let password=formState.values.password;
+    console.log(email, password);
+    try {
+      const res = await fetch(`${apiBaseUrl}/users/token`, {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (!res.ok) {
+        throw res;
+      }
+      const {
+        token,
+        user: { id },
+      } = await res.json();
+      login(token,id);
+    } catch (error) {
+      console.error(error);
+    }
   };
+
+  const login=(token)=>{
+    window.localStorage.setItem('state-skylog-app-token', token);
+    history.push('/');
+  }
 
   const hasError = field =>
     formState.touched[field] && formState.errors[field] ? true : false;
 
   return (
     <div className={classes.root}>
-      <Grid
-        className={classes.grid}
-        container
-      >
-        <Grid
-          className={classes.quoteContainer}
-          item
-          lg={5}
-        >
+      <Grid className={classes.grid} container>
+        <Grid className={classes.quoteContainer} item lg={5}>
           <div className={classes.quote}>
             <div className={classes.quoteInner}>
-              <Typography className={classes.quoteText}   variant="h1" >  TODO add quote </Typography>
+              <Typography className={classes.quoteText} variant="h1"> Once you tasted the taste of Sky, you will forever look up. </Typography>
             </div>
           </div>
         </Grid>
@@ -213,8 +228,7 @@ const SignIn = props => {
                 </Grid>
                 <Typography align="center" className={classes.sugestion} color="textSecondary" variant="body1" > or login with email address
                 </Typography>
-                <TextField className={classes.textField} error={hasError('email')} fullWidth
-                helperText={  hasError('email') ? formState.errors.email[0] : null}
+                <TextField className={classes.textField} error={hasError('email')} fullWidth  helperText={  hasError('email') ? formState.errors.email[0] : null}
                   label="Email address" name="email" onChange={handleChange} type="text" value={formState.values.email || ''} variant="outlined" />
                 <TextField className={classes.textField} error={hasError('password')} fullWidth helperText={ hasError('password') ? formState.errors.password[0] : null }
                   label="Password" name="password" onChange={handleChange} type="password" value={formState.values.password || ''} variant="outlined" />
